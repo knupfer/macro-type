892021764baf2e624776cb48f574f4ed5ff48adc
knupfer <fknupfer@gmail.com>
2014-07-02 17:11:40 +0200

    Working progn to test different pagesizes manually

--- macro-type.el
+++ macro-type.el
@@ -1,31 +1,38 @@
-(defun mt-overfullness (mt-log-file-name)
+(defun mt-overfullness (mt-log)
   (with-temp-buffer
     (let ((mt-overfull-boxes 0))
-      (insert-file-contents mt-log-file-name)
+      (insert mt-log)
+      (goto-char (point-min))
       (while (re-search-forward
               "^Overfull \\\\hbox (\\([[:digit:]\.]+\\)pt too wide).*" nil t)
         (setq mt-overfull-boxes
               (+ mt-overfull-boxes (string-to-number (match-string 1)))))
       mt-overfull-boxes)))
 
-(defun mt-change-pagesize (mt-tex-file-name)
-  (interactive)
+(defun mt-change-pagesize (mt-tex-file-name mt-margin-increase)
   (with-temp-buffer
     (insert-file-contents mt-tex-file-name)
     (re-search-forward
      "\\\\begin{document}" nil t)
-    (replace-match "
+    (replace-match (concat "
 %%%%%%%%%%%%%%% Macro-type %%%%%%%%%%%%%%%%%
-    \\\\addtolength{\\\\oddsidemargin}{-20.0mm}
-    \\\\addtolength{\\\\evensidemargin}{-20.0mm}
-    \\\\addtolength{\\\\textwidth}{40.0mm}
+    \\\\addtolength{\\\\oddsidemargin }{ "
+                           (number-to-string mt-margin-increase) "mm}
+    \\\\addtolength{\\\\evensidemargin}{ "
+                           (number-to-string mt-margin-increase) "mm}
+    \\\\addtolength{\\\\textwidth     }{"
+                           (number-to-string (* -2 mt-margin-increase)) "mm}
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-\\\\begin{document}" nil nil)
-    (write-file (concat "tmp.macro-type." mt-tex-file-name))))
+\\\\begin{document}") nil nil)
+    (write-file "/tmp/tmp.macro-type.tex")))
 
-(shell-command "pdflatex -interaction nonstopmode -draftmode -jobname tmp.macro-type tmp.macro-type.pride-and-prejudice.tex" t)
-;;(call-process "pdflatex" "tmp.macro-type.pride-and-prejudice.tex" t nil "-interaction nonstopmode")
+(defun mt-pdflatex (mt-tex-file-name)
+  (with-temp-buffer
+    (shell-command (concat "pdflatex -output-directory /tmp -draftmode -interaction nonstopmode -jobname tmp.macro-type " mt-tex-file-name) t)
+    (buffer-string)))
 
-(mt-change-pagesize "pride-and-prejudice.tex")
-(mt-overfullness "pride-and-prejudice.log")
+(progn
+  (mt-change-pagesize "pride-and-prejudice.tex" 0.105)
+  (mt-overfullness (mt-pdflatex "/tmp/tmp.macro-type.tex")))
+
