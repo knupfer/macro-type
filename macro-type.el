a38ba89f2d3caf3801d1bdcdd5712aee6066f008
knupfer <fknupfer@gmail.com>
2014-07-02 15:01:21 +0200

    Working function to collect hboxes

--- macro-type.el
+++ macro-type.el
@@ -1,4 +1,13 @@
-(with-temp-buffer (insert-file-contents "pride-and-prejudice.log")
-                  (while (re-search-forward "Overfull" nil t)
-                    (replace-match "whackiness" nil nil))
-                  (write-file "testbed.log"))
+(defun mt-overfullness ()
+  (interactive)
+  (with-temp-buffer
+    (let ((mt-overfull-boxes 0))
+      (insert-file-contents "pride-and-prejudice.log")
+      (while (re-search-forward
+              "^Overfull \\\\hbox (\\([[:digit:]\.]+\\)pt too wide).*" nil t)
+        (setq mt-overfull-boxes
+              (+ mt-overfull-boxes (string-to-number (match-string 1)))))
+      (message (number-to-string mt-overfull-boxes)))))
+
+
+
