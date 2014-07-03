6050459c3386b0dd8cb2ec4ad08a7a954f1510ee
knupfer <fknupfer@gmail.com>
2014-07-03 13:28:54 +0200

    Add license

--- macro-type.el
+++ macro-type.el
@@ -1,3 +1,25 @@
+;;; macro-type.el --- optimize margins of tex-files
+
+;; Copyright (C) 2014 Florian Knupfer
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+;; Author: Florian Knupfer
+
+
+;;; Code:
 (defun mt-overfullness (mt-log)
   (setq mt-underfull-boxes 0)
   (with-temp-buffer
